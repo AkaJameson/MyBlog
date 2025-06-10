e458d950371df3c5ab4662e416a39c1d4dca3b66
辛京爵 <xinjingjue@ganweisoft.com>
2025-06-10 22:55:39 +0800

    feat:更新

---
+++ src/MyBlog.Web.Client/Shared/Footer.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyBlog.Web.Client.Shared
+{
+    public class FooterBase: ComponentBase
+    {
+        protected DateTime CurrentTime = DateTime.Now;
+        private Timer? timer;
+
+        protected override void OnInitialized()
+        {
+            timer = new Timer(UpdateTime, null, 0, 1000); // 每秒更新
+        }
+
+        private void UpdateTime(object? state)
+        {
+            CurrentTime = DateTime.Now;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+}
