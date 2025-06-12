eea05c2344ab69ab0143656327f84f3e6fe4f6bb
辛京爵 <xinjingjue@ganweisoft.com>
2025-06-12 16:31:30 +0800

    feat:提交

---
+++ src/MyBlog.Web.Client/Shared/ArticleCard.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using MyBlog.Models.Dto;
+
+namespace MyBlog.Web.Client.Shared
+{
+    public partial class ArticleCard
+    {
+        [Parameter] public ArticleInfo Article { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        private void NavigateToDetail(int articleId)
+        {
+            NavigationManager.NavigateTo($"/view/article/{articleId}");
+        }
+    }
+}
