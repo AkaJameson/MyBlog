e1cf965500ddc45fc7fd66dc4e941eea8d110ae2
辛京爵 <xinjingjue@ganweisoft.com>
2025-06-12 23:34:20 +0800

    feat:添加热力图

---
+++ src/MyBlog.WebApi/Controllers/HotMapController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Services.Repository;
+using MyBlog.Utilites;
+
+namespace MyBlog.WebApi.Controllers
+{
+    [ApiController]
+
+    public class HotMapController : DefaultController
+    {
+        private readonly HotMapRepository _hotMapRepository;
+
+        public HotMapController(HotMapRepository hotMapRepository)
+        {
+            _hotMapRepository = hotMapRepository;
+        }
+        [HttpGet]
+        public async Task<OperateResult> GetHotMap([FromQuery] int year)
+        {
+            if (!ModelState.IsValid)
+            {
+                return OperateResult.Failed("参数错误");
+            }
+            var hotMap = _hotMapRepository.GetHotMapAsync(year);
+            return OperateResult.Successed(hotMap);
+        }
+    }
+}
