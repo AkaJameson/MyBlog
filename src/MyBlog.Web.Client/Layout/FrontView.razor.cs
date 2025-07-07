ab738e8bc2852bcd92a8c8caaac81e9ed5d63591
辛京爵 <xinjingjue@ganweisoft.com>
2025-07-07 18:04:22 +0800

    fix:添加备案号

--- src/MyBlog.Web.Client/Layout/FrontView.razor.cs
+++ src/MyBlog.Web.Client/Layout/FrontView.razor.cs
@@ -7,15 +7,14 @@ namespace MyBlog.Web.Client.Layout
 {
     public partial class FrontView
     {
-        protected DateTime CurrentTime = DateTime.Now;
         [Inject]
         private ArticleApiService ArticleApiService { get; set; }
         [Inject]
         private HotMapApiService HotMapApiService { get; set; }
         [Inject]
-        private NavigationManager NavigationManager { get; set; }
+        private IConfiguration Configuration { get; set; }
         private List<ArticleInfo> ArticleInfos { get; set; }
-        private Timer? timer;
+        private string BeiAnHao { get; set; }
         private BlogDetailCount BlogDetailCount { get; set; } = new();
         protected override async Task OnParametersSetAsync()
         {
@@ -25,6 +24,7 @@ namespace MyBlog.Web.Client.Layout
                 ArticleInfos = result.Data;
             }
             await GetBlogDetailCount();
+            BeiAnHao = Configuration.GetValue<string>("BeiAnHao")?? "";
 
         }
         private async Task<Dictionary<DateTime, int>> QueryHotMap(int year)
@@ -32,11 +32,6 @@ namespace MyBlog.Web.Client.Layout
             var result = await HotMapApiService.GetHotMapData(year);
             return result;
         }
-        protected override void OnInitialized()
-        {
-            timer = new Timer(UpdateTime, null, 0, 1000); // 每秒更新
-        }
-
         protected async Task GetBlogDetailCount()
         {
             var result = await ArticleApiService.GetBlogDetailCount();
@@ -44,17 +39,6 @@ namespace MyBlog.Web.Client.Layout
             {
                 BlogDetailCount = result.Data;
             }
-        }
-
-        private void UpdateTime(object? state)
-        {
-            CurrentTime = DateTime.Now;
-            InvokeAsync(StateHasChanged);
-        }
-
-        public void Dispose()
-        {
-            timer?.Dispose();
         }
     }
 }
